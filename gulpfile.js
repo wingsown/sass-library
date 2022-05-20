215d4d2968b36be7b05c0d8b4c2f2101c3bd7889
Wilson <wj.buena@gmail.com>
2022-05-20 09:58:48 +0800

    Purge css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
     return src('sass/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
 }
 
